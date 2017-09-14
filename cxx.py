25e84e2f367a2648a1f6a95b69449dee56dac3bf
SSE4 <tomskside@gmail.com>
2017-09-14 16:08:14 +0700

    - remove wf.py and wf++.py, use just cc.py and cxx.py

--- cxx.py
+++ cxx.py
@@ -4,5 +4,5 @@ import winfigure
 import sys
 
 if __name__ == '__main__':
-	status = winfigure.run(language='c', args=sys.argv[1:])
+	status = winfigure.run(language='cxx', args=sys.argv[1:])
 	sys.exit(status)
--- wf++.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import winfigure
-import sys
-
-if __name__ == '__main__':
-	status = winfigure.run(language='c++', args=sys.argv[1:])
-	sys.exit(status)
